7cfa9ea965e3774b74265287dac2ad82a51e2ed8
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-28 19:42:43 +0100

    add background color

--- src/screens/auth/signin/SignInPage.tsx
+++ src/screens/auth/signin/SignInPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { MouseEvent, useState } from "react"
 import profileIcon from "../../../assets/svg/profile.svg"
 import eyesOffIcon from "../../../assets/svg/eyesoff-outlined.svg"
 import eyesOnIcon from "../../../assets/svg/eyeson-outlined.svg"
@@ -11,40 +11,55 @@ const SignInPage = () => {
         setshowPassword(!showPassword)
     }
 
+    const handleLogin = (e: MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+        console.log("LOGIN")
+        console.log("password ===>>>", password)
+    }
+
   return (
     <>
-        <div className="flex flex-col items-center gap-4 font-semibold">
-            <p>{`Swify--->pay`}</p>
-            <div className="flex items-center justify-center w-10 h-10 border-1 border-gray-200 bg-gray-100 rounded-full"><img src={profileIcon} alt="" /></div>
-            <p>STEPHEN(ste...@gmail.com)</p>
-        </div>
+    {/* <div className="mx-auto my-10 max-w-[400px] border-2"> */}
+    <div className="mt-12 flex justify-center">
+        <div className="w-full max-w-[400px]">
+            <div className="flex flex-col items-center gap-4 font-semibold">
+                <p>{`Swify--->pay`}</p>
+                <div 
+                    className="flex items-center justify-center w-10 h-10 border-1 border-gray-200 bg-gray-100 rounded-full">
+                    <img src={profileIcon} alt="" />
+                </div>
+                <p>STEPHEN(ste...@gmail.com)</p>
+            </div>
 
-        <div className="px-6 mt-4 flex-center">
-            <form action="" className="flex flex-col gap-4">
-                <div className="form-group relative">
-                    <input 
-                        className="p-2 w-full border-2 border-gray-200 rounded-[8px]" 
-                        placeholder="Enter password"
-                        type={showPassword ? "text" : "password"}
-                        onChange={(e)=> setpassword(e.target.value)}
-                        value={password}
-                    />
-                    <div className="my-2 flex justify-end text-xs capitalize">
-                        forgot password
+            <div className="px-6 mt-4 flex-center">
+                <form className="flex flex-col gap-4">
+                    <div className="form-group relative">
+                        <input 
+                            className="appearance-none text-gray-700 focus:outline-none focus:shadow-outline focus:border-blue-400  p-2 w-full border-2 border-gray-200 rounded-[8px]" 
+                            placeholder="Enter password"
+                            type={showPassword ? "text" : "password"}
+                            onChange={(e)=> setpassword(e.target.value)}
+                            value={password}
+                        />
+                        <div className="my-2 flex justify-end text-xs capitalize">
+                            forgot password
+                        </div>
+                        <img src={showPassword? eyesOnIcon: eyesOffIcon} alt="" 
+                            className="absolute top-3 right-2"
+                            onClick={togglePasswordVisibility}
+                        />  
                     </div>
-                    <img src={showPassword? eyesOnIcon: eyesOffIcon} alt="" 
-                        className="absolute top-3 right-2"
-                        onClick={togglePasswordVisibility}
-                    />  
-                </div>
 
-                <div className="form-group">
-                    <input className="p-2 text-white w-full bg-amber-300 border-2 border-amber-300 rounded-[8px]" type="submit" name="" id="" />
-                </div>
-            </form>
+                    <button
+                        onClick={(e)=> handleLogin(e)}
+                        className="p-2 text-white w-full bg-amber-300 border-2 border-amber-300 rounded-[8px]"
+                    >
+                        <span>Login</span>
+                    </button>
+                </form>
+            </div>
         </div>
-
-
+    </div>
     </>
   )
 }
