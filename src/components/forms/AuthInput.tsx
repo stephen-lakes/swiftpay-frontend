aabdc1d5985a05a00c86f19dc7b21de6b2c69661
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-28 19:43:09 +0100

    AuthInput

--- src/components/forms/AuthInput.tsx
+++ src/components/forms/AuthInput.tsx
@@ -1,36 +1,37 @@
 import { useState } from "react"
-import profileIcon from "../../assets/svg/profile.svg"
+import eyesOffIcon from "../../assets/svg/eyesoff-outlined.svg"
+import eyesOnIcon from "../../assets/svg/eyeson-outlined.svg"
+
 
 
 const AuthInput = () => {
     const [password, setpassword] = useState('')
     const [showPassword, setshowPassword] = useState(false)
     
-        const togglePasswordVisibility = ()=> {
-            setshowPassword(!showPassword)
-        }
-    
-  return (
-    <>
-    <div className="form-group relative">
-        <input 
-            className="p-2 w-full border-2 border-gray-200 rounded-[8px]" 
-            placeholder="Enter password"
-            type={showPassword ? "text" : "password"}
-            onChange={(e)=> setpassword(e.target.value)}
-            value={password}
-        />
-        <div className="my-2 flex justify-end text-xs capitalize">
-            forgot password
-        </div>
-        <img src={profileIcon} alt="" 
-            className="absolute top-3 right-2"
-            onClick={togglePasswordVisibility}
-        />  
-    </div>
+    const togglePasswordVisibility = ()=> {
+        setshowPassword(!showPassword)
+    }
 
-    </>
-  )
+    return (
+        <>
+            <div className="form-group relative">
+                <input 
+                    className="p-2 w-full border-2 border-gray-200 rounded-[8px]" 
+                    placeholder="Enter password"
+                    type={showPassword ? "text" : "password"}
+                    onChange={(e)=> setpassword(e.target.value)}
+                    value={password}
+                />
+                <div className="my-2 flex justify-end text-xs capitalize">
+                    forgot password
+                </div>
+                <img src={showPassword? eyesOnIcon: eyesOffIcon} alt="" 
+                    className="absolute top-3 right-2"
+                    onClick={togglePasswordVisibility}
+                />  
+            </div>
+        </>
+    )
 }
 
 export default AuthInput
