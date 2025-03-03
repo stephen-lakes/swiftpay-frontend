c8593cfc46e3ee1990d742ced3cb9a3e2d4959ed
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-03 17:04:53 +0100

    Page routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        
         <Routes>
           <Route path="/" element={<AuthIndex />} />
           <Route path="/dashboard" element={<Home />} />
