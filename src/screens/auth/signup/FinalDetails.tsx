41d4e53d1be96f9cefd88a844120603768c60fe3
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-03 16:01:50 +0100

    feat: Add types to FinalDetails component and integrate Tailwind CSS styles
    - Define types for FinalDetails props, including SignUpData and event
      handlers.
    - Update the FinalDetails component to use React.FC and TypeScript
        types.
    - Apply Tailwind CSS classes for consistent styling.

---
+++ src/screens/auth/signup/FinalDetails.tsx
@@ -0,0 +1,74 @@
+import React, { ChangeEvent, FormEvent } from 'react';
+
+// Define the types for the props
+interface SignUpData {
+  firstname: string;
+  lastname: string;
+  phoneNumber?: string; // Optional field
+}
+
+interface FinalDetailsProps {
+  signUpData: SignUpData;
+  handleNextPage: () => void;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const FinalDetails: React.FC<FinalDetailsProps> = ({ signUpData, handleNextPage, handleChange }) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("FORM DATA", signUpData);
+    handleNextPage();
+  }
+
+  return (
+    <>
+      <div className="text-gray-700 text-3xl font-bold">FinalDetails</div>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div className="form-group relative">
+          <label className="text-gray-500">Firstname</label>
+          <input
+            className="appearance-none text-gray-700 focus:outline-none focus:shadow-outline focus:border-blue-400 p-2 w-full border-2 border-gray-200 rounded-[8px]"
+            placeholder="firstname"
+            name="firstname"
+            value={signUpData.firstname}
+            onChange={handleChange}
+            type="text"
+          />
+        </div>
+
+        <div className="form-group relative">
+          <label className="text-gray-500">Lastname</label>
+          <input
+            className="appearance-none text-gray-700 focus:outline-none focus:shadow-outline focus:border-blue-400 p-2 w-full border-2 border-gray-200 rounded-[8px]"
+            placeholder="lastname"
+            name="lastname"
+            value={signUpData.lastname}
+            onChange={handleChange}
+            type="text"
+          />
+        </div>
+
+        <div className="form-group relative">
+          <label className="text-gray-500">Phone number (optional)</label>
+          <input
+            className="appearance-none text-gray-700 focus:outline-none focus:shadow-outline focus:border-blue-400 p-2 w-full border-2 border-gray-200 rounded-[8px]"
+            placeholder="phone"
+            name="phoneNumber"
+            value={signUpData.phoneNumber}
+            onChange={handleChange}
+            type="text"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="p-2 text-white w-full bg-amber-300 border-2 border-amber-300 rounded-[8px]"
+        >
+          <span>Finish</span>
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default FinalDetails;
