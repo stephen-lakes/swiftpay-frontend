620a4afa8f4497dda842710390a3dbe47f3ebe34
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-05 17:38:27 +0100

    feat: add nextPage prop

--- src/components/cards/SwiftPayMiniCard.tsx
+++ src/components/cards/SwiftPayMiniCard.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 interface SwiftPayMiniCardProp {
     icon: string;
     title: string;
+    nextPage?: string
 }
 
-const SwiftPayMiniCard: React.FC<SwiftPayMiniCardProp> = ({ icon, title}) => {
+const SwiftPayMiniCard: React.FC<SwiftPayMiniCardProp> = ({ icon, title, nextPage}) => {
+  const navigate = useNavigate()
+  const goToPage = () => {
+    navigate(`/${nextPage}`)
+  }
+
   return (
     <>
-        <div className="flex flex-col justify-center items-center bg-gray-200 w-16 h-16 rounded-2xl cursor-pointer">
+        <div 
+          onClick={goToPage}
+          className="flex flex-col justify-center items-center bg-gray-200 w-16 h-16 rounded-2xl cursor-pointer">
             <div>
               <img src={icon} alt="" />
             </div>
