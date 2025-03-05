476e547e1a410919e96ef010eb2fba2d65a20e99
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-05 17:59:25 +0100

    template for transfer page

--- src/screens/SwiftPayTransfer.tsx
+++ src/screens/SwiftPayTransfer.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Loader from '../components/Loader'
 
-const SwiftPayTransfer = () => {
+const SwiftPayTransfer: React.FC = () => {
   return (
-    <div>SwiftPayTransfer</div>
+    <>
+      <h1>SwiftPayTransfer</h1>
+      <form>
+        <div className="flex flex-col gap-3">
+          <input
+              className="p-2 w-full border-2 border-gray-200 rounded-[8px]"
+              placeholder='username/email/phone number'
+              type="text" 
+          />
+
+          <Loader />
+
+          <div className="text-center uppercase">
+            James Smith
+          </div>
+
+          <div className="p-3 flex items-center justify-center bg-amber-400 text-white">
+            CONFIRM
+          </div>
+
+        </div>
+      </form>
+
+
+      <div className="pin keyboard">
+        Key board comes up to comfirm
+        transaction with PIN.
+        This component can be moved to its component
+      </div>
+    </>
   )
 }
 
