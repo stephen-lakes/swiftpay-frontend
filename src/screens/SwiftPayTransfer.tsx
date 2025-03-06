7a775e2eb8021eb930ef5fc0870610c14bb20012
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-07 00:45:00 +0100

    Transfer screen

--- src/screens/SwiftPayTransfer.tsx
+++ src/screens/SwiftPayTransfer.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Loader from '../components/Loader'
 
 const SwiftPayTransfer: React.FC = () => {
+  const [verifyReceiver, setVerifyReceiver] = useState(false);
   return (
     <>
-      <h1>SwiftPayTransfer</h1>
+    <div>
+      <h1 className="mt-10 mb-3 text-center">SwiftPayTransfer</h1>
       <form>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 px-3">
           <input
               className="p-2 w-full border-2 border-gray-200 rounded-[8px]"
               placeholder='username/email/phone number'
               type="text" 
           />
 
-          <Loader />
+          {verifyReceiver && <Loader />}
+
 
           <div className="text-center uppercase">
             James Smith
           </div>
 
-          <div className="p-3 flex items-center justify-center bg-amber-400 text-white">
-            CONFIRM
+          <div className="p-3 flex items-center justify-center bg-amber-400 rounded text-white">
+            PROCEED
           </div>
 
         </div>
@@ -32,6 +35,7 @@ const SwiftPayTransfer: React.FC = () => {
         transaction with PIN.
         This component can be moved to its component
       </div>
+    </div>
     </>
   )
 }
