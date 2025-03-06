6688c86ad57206276c87cbd052f51bae4ff1d827
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-06 23:44:26 +0100

    feat: render recent transactions

--- src/screens/dashboard/Home.tsx
+++ src/screens/dashboard/Home.tsx
@@ -1,8 +1,9 @@
+import React from "react"
 import SendIcon from ".././../assets/svg/send.svg"
 import BankIcon from ".././../assets/svg/bank.svg"
 import SwiftPayMiniCard from "../../components/cards/SwiftPayMiniCard"
 import SwiftPayAccountBalanceCard from "../../components/cards/SwiftPayAccountBalanceCard"
-import React from "react"
+import SwiftPayTransactionHistoryList from "../../components/cards/SwiftPayTransactionHistoryCard"
 
 
 const Home:React.FC = () => {
@@ -21,6 +22,18 @@ const Home:React.FC = () => {
           <SwiftPayMiniCard icon={BankIcon} title="electricity" nextPage="electricity" />
         </div>
 
+        <div className="transactions pb-24 p-2">
+          <header className="recent-transactions-header mt-6 flex">
+            <h1 className="font-bold">Recent Transactions</h1>
+            <div className="ml-auto">See All</div>
+          </header>
+          <input 
+            className="my-2 appearance-none text-gray-700 focus:outline-none focus:shadow-outline focus:border-blue-400 p-2 w-full border-2 border-gray-200 rounded-[8px]"
+            placeholder="Search with name..." type="text" name="" id="" 
+          />
+
+         <SwiftPayTransactionHistoryList />
+        </div>
       </div>
     </>
   )
