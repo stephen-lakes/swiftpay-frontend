71f4d1d27a29578dee8c45638aef0315a008327d
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-06 01:23:14 +0100

    feat: Optimize ConfirmEmail component
    - Simplified OTP verification logic for efficiency.
    - Improved focus handling and navigation between OTP inputs.
    - Enhanced component readability and maintainability.
    - Ensured consistent styling and proper typing throughout the component.

--- src/screens/auth/signup/ConfirmEmail.tsx
+++ src/screens/auth/signup/ConfirmEmail.tsx
@@ -26,64 +26,55 @@ const ConfirmEmail: React.FC<ConfirmEmailProps> = ({ handleNextPage, signUpData 
       inputs.current[nextInput]?.focus();
     }
 
-     // Automatically verify OTP after last input is entered
-     if (index === inputs.current.length - 1 && value.length === maxLength) {
+    // Automatically verify OTP after last input is entered
+    if (index === inputs.current.length - 1 && value.length === maxLength) {
       verifyOtp();
     }
-
   };
 
-  const getOtpValues = (): string => {
-    return inputs.current.map(input => input?.value || "").join('');
-  };
+  const getOtpValues = (): string => inputs.current.map(input => input?.value || "").join('');
 
   const verifyOtp = () => {
-    const otpValues = inputs.current.map(input => input?.value || "");
-    const allInputsFilled = otpValues.every(value => value !== "");
-
-    if (allInputsFilled) {
-      const otp = otpValues.join('');
+    const otp = getOtpValues();
+    if (otp.length === inputs.current.length) {
       console.log("OTP:", otp);
+      setVerifyingOTP(true);
       setTimeout(() => {
         setVerifyingOTP(false);
         handleNextPage();
       }, 2000);
     } else {
-      setVerifyingOTP(false);
       alert("Please fill all OTP fields.");
+      setVerifyingOTP(false);
     }
   };
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center gap-4 mt-12">
-        <div className="text-gray-700 text-3xl font-bold">Confirm Email</div>
-        <form>
-          <div className="my-4 text-center">{hideEmail(signUpData.email)}</div>
-          <div className="flex items-center gap-4">
-            {Array(6).fill(0).map((_, index) => (
-              <input
-                key={index}
-                type="text"
-                className="appearance-none text-center text-gray-700 focus:outline-none focus:shadow-outline focus:border-blue-400 p-2 w-8 h-12 md:w-12 md:h-12 border-2 border-gray-200 rounded-[8px]"
-                maxLength={1}
-                ref={(el) => { inputs.current[index] = el }}
-                onChange={(e) => handleChange(e, index)}
-              />
-            ))}
-          </div>
-          <div className="text-xs mt-2">
-            <span className="text-[#3B82F6] flex justify-end cursor-pointer">resend OTP</span>
-          </div>
 
-          {/* <button className="mt-4 p-2 text-white w-full bg-amber-300 border-2 border-amber-300 rounded-[8px]" type="submit">Verify</button> */}
-          <div className="mt-4">
-            {verifyingOTP && <Loader />}
-          </div>
-          
-        </form>
-      </div>
-    </>
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 mt-12">
+      <div className="text-gray-700 text-3xl font-bold">Confirm Email</div>
+      <form>
+        <div className="my-4 text-center">{hideEmail(signUpData.email)}</div>
+        <div className="flex items-center gap-4">
+          {Array(6).fill(0).map((_, index) => (
+            <input
+              key={index}
+              type="text"
+              className="appearance-none text-center text-gray-700 focus:outline-none focus:shadow-outline focus:border-blue-400 p-2 w-8 h-12 md:w-12 md:h-12 border-2 border-gray-200 rounded-[8px]"
+              maxLength={1}
+              ref={(el) => { inputs.current[index] = el; }}
+              onChange={(e) => handleChange(e, index)}
+            />
+          ))}
+        </div>
+        <div className="text-xs mt-2">
+          <span className="text-[#3B82F6] flex justify-end cursor-pointer">resend OTP</span>
+        </div>
+        <div className="mt-4">
+          {verifyingOTP && <Loader />}
+        </div>
+      </form>
+    </div>
   );
-}
+};
 
 export default ConfirmEmail;
