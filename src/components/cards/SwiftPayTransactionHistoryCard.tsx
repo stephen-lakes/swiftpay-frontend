cc99ca529d387ea3f6f0ee67bbba9b64cecb37cf
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-06 13:18:42 +0100

    feat: custom component to render transaction item

---
+++ src/components/cards/SwiftPayTransactionHistoryCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+interface SwiftPayTransactionHistoryCardProp {
+  naration: string;
+  date: string;
+  time: string;
+  amount: string;
+  type: string;
+  service: string;
+}
+
+const SwiftPayTransactionHistoryCard: React.FC<SwiftPayTransactionHistoryCardProp> = ({naration, date, time, amount, type, service}) => {
+  return (
+    <>
+        <div className="flex items-center gap-2 transaction-list-item">
+          <div className={`${type ==="debit" ? "bg-red-300": "bg-green-300"} w-10 h-10 flex items-center justify-center rounded-xs`}>A</div>
+          <div className="text-xs font-light">
+            <p className="text-sm font-semibold">{naration}</p>
+            <p>{date}</p>
+            <p>{time}</p>
+          </div>
+
+          <div className="ml-auto">
+            <div className="text-sm font-semibold">{`${type==="debit" ? "-" : type==="credit" ? "+" : ""}`}₦{amount}</div>
+            <p className="text-xs text-right font-light">{service}</p>
+          </div>
+          
+        </div>
+    </>
+  )
+}
+
+export default SwiftPayTransactionHistoryCard
