1f16d295420b887929c599d45ebe65006205f6e3
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-06 23:43:47 +0100

    feat: Create component to render the list of transaction cards

--- src/components/cards/SwiftPayTransactionHistoryCard.tsx
+++ src/components/cards/SwiftPayTransactionHistoryCard.tsx
@@ -12,7 +12,7 @@ interface SwiftPayTransactionHistoryCardProp {
 const SwiftPayTransactionHistoryCard: React.FC<SwiftPayTransactionHistoryCardProp> = ({naration, date, time, amount, type, service}) => {
   return (
     <>
-        <div className="flex items-center gap-2 transaction-list-item">
+        <div className="flex items-center gap-2 transaction-list-item py-2">
           <div className={`${type ==="debit" ? "bg-red-300": "bg-green-300"} w-10 h-10 flex items-center justify-center rounded-xs`}>A</div>
           <div className="text-xs font-light">
             <p className={`text-sm font-semibold ${service.toLocaleLowerCase() === "transfer" ? "uppercase": ""}`}>{naration}</p>
@@ -30,4 +30,43 @@ const SwiftPayTransactionHistoryCard: React.FC<SwiftPayTransactionHistoryCardPro
   )
 }
 
-export default SwiftPayTransactionHistoryCard
+const transactions = [
+  {
+    id: 1,
+    naration: 'MTN Data',
+    date: 'March 5, 2025',
+    time: '5:12:34 AM',
+    amount: '1500',
+    type: 'debit',
+    service: 'Data',
+  },
+  {
+    id: 2,
+    naration: 'STEPHEN OLAMILEKAKN',
+    date: 'March 5, 2025',
+    time: '5:12:15 AM',
+    amount: '1500',
+    type: 'credit',
+    service: 'transfer',
+  },
+  {
+    id: 3,
+    naration: 'STEPHEN OLAMILEKAKN',
+    date: 'February 14, 2025',
+    time: '9:17:42 AM',
+    amount: '250',
+    type: 'credit',
+    service: 'transfer',
+  },
+]
+
+
+const SwiftPayTransactionHistoryList: React.FC= () => {
+  return(
+    <>
+      {transactions.map(transaction => <SwiftPayTransactionHistoryCard key={transaction.id} naration={transaction.naration} date={transaction.date} time={transaction.time} amount={transaction.amount} type={transaction.type} service={transaction.service} />)}
+    </>
+  )
+}
+
+export default SwiftPayTransactionHistoryList
