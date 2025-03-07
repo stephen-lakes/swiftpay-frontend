af918a04a55339e3399c498fc404ab442498042e
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-07 19:47:20 +0100

    chore: change UI colors

--- src/components/cards/SwiftPayTransactionHistoryCard.tsx
+++ src/components/cards/SwiftPayTransactionHistoryCard.tsx
@@ -13,7 +13,7 @@ const SwiftPayTransactionHistoryCard: React.FC<SwiftPayTransactionHistoryCardPro
   return (
     <>
         <div className="flex items-center gap-2 transaction-list-item py-2">
-          <div className={`${type ==="debit" ? "bg-red-300": "bg-green-300"} w-10 h-10 flex items-center justify-center rounded-xs`}>A</div>
+          <div className={`${type ==="debit" ? "bg-[#FDE8EA]": "bg-[#E8F1FD]"} w-10 h-10 flex items-center justify-center rounded-xs`}>A</div>
           <div className="text-xs font-light">
             <p className={`text-sm font-semibold ${service.toLocaleLowerCase() === "transfer" ? "uppercase": ""}`}>{naration}</p>
             <p>{date}</p>
@@ -21,7 +21,7 @@ const SwiftPayTransactionHistoryCard: React.FC<SwiftPayTransactionHistoryCardPro
           </div>
 
           <div className="ml-auto">
-            <div className="text-sm font-semibold">{`${type==="debit" ? "-" : type==="credit" ? "+" : ""}`}₦{amount}</div>
+            <div className={`${type==="debit"? "text-[#ED2F46]": "text-[#2F80ED]"} text-sm font-semibold`}>{`${type==="debit" ? "-" : type==="credit" ? "+" : ""}`}₦{amount}</div>
             <p className="text-xs text-right font-light capitalize">{service}</p>
           </div>
           
