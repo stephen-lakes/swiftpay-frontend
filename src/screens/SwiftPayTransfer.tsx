ec2184e9693e851b39603a737d27d18549113bdf
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-12 00:35:30 +0100

    Transfer Screen

--- src/screens/SwiftPayTransfer.tsx
+++ src/screens/SwiftPayTransfer.tsx
@@ -3,15 +3,16 @@ import Loader from '../components/Loader'
 import SwiftPayScreenHeader from '../components/SwiftPayScreenHeader';
 
 const SwiftPayTransfer: React.FC = () => {
-  const [verifyReceiver, setVerifyReceiver] = useState(false);
+  const [verifyReceiver] = useState(false);
   return (
     <>
       <SwiftPayScreenHeader screenTitle="Swiftpay Transfer"/>
 
-      <form>
+      <form className='p-3'>
         <div className="flex flex-col gap-3 px-3">
           <input
-              className="p-2 w-full border-2 border-gray-200 rounded-[8px]"
+              // className="p-2 w-full border-2 border-gray-200 rounded-[8px]"
+              className="appearance-none text-gray-700 focus:outline-none focus:shadow-outline focus:border-purple-400 p-2 w-full border-2 border-gray-200 rounded-[8px]"
               placeholder='account number or email or phone number'
               type="text" 
           />
@@ -26,13 +27,13 @@ const SwiftPayTransfer: React.FC = () => {
           <div>
             REMARK(Optional)
             <input
-                className="p-2 w-full border-2 border-gray-200 rounded-[8px]"
+                className="p-2 w-full border-2 active:border-gray-200 border-gray-200 rounded-[8px]"
                 type="text" 
             />
           </div>
 
 
-          <div className="p-3 flex items-center justify-center bg-purple-600 text-white uppercase cursor-pointer rounded">
+          <div className="mt-10 h-10 p-3 flex items-center justify-center bg-purple-600 text-white uppercase cursor-pointer rounded">
             PROCEED
           </div>
 
