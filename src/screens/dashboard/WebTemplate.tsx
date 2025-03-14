baafb65877aa60e0a6ba77be809ed3e3baf0a0ee
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-14 16:30:23 +0100

    remove font weight
    add hover effect to navlink text

--- src/screens/dashboard/WebTemplate.tsx
+++ src/screens/dashboard/WebTemplate.tsx
@@ -1,6 +1,7 @@
 import HomeIcon from "../../assets/svg/home.svg"
 import CardIcon from "../../assets/svg/creditcard.svg"
 import UserIcon from "../../assets/svg/profile.svg"
+import Home from "./Home"
 
 const SwiftPayDashboard = () => {
   return (
@@ -16,16 +17,16 @@ interface NavLinkProps {
   icon?: string;
 }
 const NavLink: React.FC<NavLinkProps> = ({ href, label, icon }) => (
-  <a href={href} className="flex flex-col sm:justify-center lg:justify-start lg:flex-row gap-1.5 lg:gap-2.5 items-center px-2 py-2.5 hover:text-[#6D28D9] bg-amber-200 lg:mt-2">
+  <a href={href} className="flex flex-col sm:justify-center lg:justify-start lg:flex-row gap-1.5 lg:gap-2.5 items-center px-2 py-2.5 lg:mt-2">
     <div className="w-6 h-6 rounded">
       <img src={icon} alt="" className="w-full h-full object-cover" />
     </div>
-    <div className="text-[#22183C] text-[14.5px]">{label}</div>
+    <div className="text-[#22183C] text-[14.5px] hover:text-[#6D28D9]">{label}</div>
   </a>
 );
 
 const Nav: React.FC = () => (
-  <nav className="flex justify-between lg:justify-start lg:flex-col fixed bottom-0 left-0 right-0 lg:static lg:h-[100vh] bg-purple-300 flex-2 !p-3">
+  <nav className="flex justify-between lg:justify-start lg:flex-col fixed bottom-0 left-0 right-0 lg:static lg:h-[100vh] flex-2 !p-3 sm:bg-[#191043] lg:bg-inherit">
     <NavLink href="" label="Home" icon={HomeIcon} />
     <NavLink href="" label="Cards" icon={CardIcon} />
     <NavLink href="" label="Profile" icon={UserIcon} />
@@ -34,14 +35,16 @@ const Nav: React.FC = () => (
 
 const SwiftPayAppLayout = () => {
   return (
-    <div className="lg:flex h-screen bg-[#EFE4FC]">
+    <div className="lg:flex lg:gap-6 h-screen bg-[#EFE4FC]">
       <Nav />
-      <div className="bg-red-300 flex-6 !p-3">
-        <div className="bg-pink-600 rounded-[.625rem] !p-3 min-h-96 h-full"></div>
-      </div>
+      <main className="mt-6 flex-6 !p-3 rounded-3xl bg-white">
+        <div className="rounded-[.625rem] !p-3 min-h-96 h-full">
+          <Home />
+        </div>
+      </main>
       <div className="bg-green-300 flex-3 !pb-40 lg:!pb-0">
         <div className="!p-3 flex flex-col gap-4">
-          TRANSACTIONS
+          QUICK INSIGHTS
           <div className="h-60 bg-red-700 rounded-[.625rem]"></div>
           <div className="h-30 bg-red-400 rounded-[.625rem]"></div>
         </div>
