1e994e7614ca0b867ec67c478b7a35e264cf4be6
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-14 11:10:43 +0100

    Refactored ThreeColumnLayout to include Nav component and optimized NavLink for icon support
    - Imported HomeIcon, CardIcon, and UserIcon
    - Added Nav component to encapsulate navigation logic
    - Enhanced NavLink component to support icons and ensure icons fill
      their containers
    - Improved code modularity and readability

--- src/screens/dashboard/WebTemplate.tsx
+++ src/screens/dashboard/WebTemplate.tsx
@@ -1,3 +1,7 @@
+import HomeIcon from "../../assets/svg/home.svg"
+import CardIcon from "../../assets/svg/creditcard.svg"
+import UserIcon from "../../assets/svg/profile.svg"
+
 const WebTemplate = () => {
   return (
     <ThreeColumnLayout />
@@ -9,22 +13,29 @@ export default WebTemplate
 interface NavLinkProps {
   href: string;
   label: string;
+  icon?: string;
 }
-const NavLink: React.FC<NavLinkProps> = ({ href, label }) => (
+const NavLink: React.FC<NavLinkProps> = ({ href, label, icon }) => (
   <a href={href} className="flex flex-col sm:justify-center lg:justify-start lg:flex-row gap-1.5 lg:gap-2.5 items-center px-2 py-2.5 hover:text-[#6D28D9] bg-amber-200 lg:mt-2">
-    <div className="w-6 h-6 bg-green-700 rounded"></div>
+    <div className="w-6 h-6 rounded">
+      <img src={icon} alt="" className="w-full h-full object-cover" />
+    </div>
     <div className="text-[#22183C] text-[14.5px]">{label}</div>
   </a>
+);
+
+const Nav: React.FC = () => (
+  <nav className="flex justify-between lg:justify-start lg:flex-col fixed bottom-0 left-0 right-0 lg:static lg:h-[100vh] bg-purple-300 flex-2 !p-3">
+    <NavLink href="" label="Home" icon={HomeIcon} />
+    <NavLink href="" label="Cards" icon={CardIcon} />
+    <NavLink href="" label="Profile" icon={UserIcon} />
+  </nav>
 );
 
 const ThreeColumnLayout = () => {
   return (
     <div className="lg:flex h-screen bg-[#EFE4FC]">
-      <nav className="flex justify-between lg:justify-start lg:flex-col fixed bottom-0 left-0 right-0 lg:static lg:h-[100vh] bg-purple-300 flex-2 !p-3">
-        <NavLink href="" label="Home" />
-        <NavLink href="" label="Cards" />
-        <NavLink href="" label="Profile" />
-      </nav>
+      <Nav />
       <div className="bg-red-300 flex-6 !p-3">
         <div className="bg-pink-600 rounded-[.625rem] !p-3 min-h-96 h-full"></div>
       </div>
